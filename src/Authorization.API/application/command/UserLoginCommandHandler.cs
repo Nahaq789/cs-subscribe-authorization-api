1c2e44677ee57d54396747d2e73ecc333b9a0850
Nahaq <naha022677@icloud.com>
2024-06-02 16:45:17 +0900

    トークン生成

--- src/Authorization.API/application/command/UserLoginCommandHandler.cs
+++ src/Authorization.API/application/command/UserLoginCommandHandler.cs
@@ -1,18 +1,26 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Authorization.API.application.crypto;
 using Authorization.API.application.dto;
 using Authorization.API.infrastructure.repository;
 using MediatR;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace Authorization.API.application.command;
 
 public class UserLoginCommandHandler : IRequestHandler<UserLoginCommand, LoginResult>
 {
     private readonly IUserAuthRepository _userAuthRepository;
+    private readonly IConfiguration _configuration;
 
     /// <param name="userAuthRepository">ユーザーオースリポジトリ</param>
-    public UserLoginCommandHandler(IUserAuthRepository userAuthRepository)
+    /// <param name="configuration">コンフィグ</param>
+    public UserLoginCommandHandler(IUserAuthRepository userAuthRepository, IConfiguration configuration)
     {
         this._userAuthRepository = userAuthRepository ?? throw new ArgumentNullException(nameof(userAuthRepository));
+        this._configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
     }
 
     /// <summary>
@@ -33,10 +41,28 @@ public class UserLoginCommandHandler : IRequestHandler<UserLoginCommand, LoginRe
                 ErrorMessage = "ログイン情報が無効です。"
             };
         }
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, command.Email),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Exp, DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds().ToString())
+        };
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetValue<string>("Jwt_Key") ?? string.Empty));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration.GetValue<string>("Jwt:Issuer"),
+            audience: _configuration.GetValue<string>("Jwt:Audience"),
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: credentials
+        );
+
         return new LoginResult
         {
             Success = true,
-            Token = ""
+            Token = new JwtSecurityTokenHandler().WriteToken(token)
         };
     }
 }
