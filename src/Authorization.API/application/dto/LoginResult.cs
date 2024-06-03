016443525d638754491d0a4912661db27d0c5e54
Nahaq <naha022677@icloud.com>
2024-06-04 08:17:57 +0900

    リフレッシュトークン記載

--- src/Authorization.API/application/dto/LoginResult.cs
+++ src/Authorization.API/application/dto/LoginResult.cs
@@ -4,5 +4,6 @@ public class LoginResult
 {
     public bool Success { get; set; }
     public string Token { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
     public string ErrorMessage { get; set; } = string.Empty;
 }
