94ba67297dd22de9128e161ebbf6f74187e2f43c
Nahaq <naha022677@icloud.com>
2024-06-04 08:18:49 +0900

    ソルト追加

--- src/Authorization.API/domain/UserAuth.cs
+++ src/Authorization.API/domain/UserAuth.cs
@@ -4,10 +4,12 @@ public class UserAuth
 {
     public string Email { get; private set; }
     public string Password { get; private set; }
+    public string Salt { get; private set; }
 
-    public UserAuth(string email, string password)
+    public UserAuth(string email, string password, string salt)
     {
         this.Email = email;
         this.Password = password;
+        this.Salt = salt;
     }
 }
