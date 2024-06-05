2efc4c0e0c458f3b69b0d4033d63245d20899099
Nahaq789 <naha022677@icloud.com>
2024-06-05 22:38:27 +0000

    fix warning

--- src/Authorization.API/infrastructure/idempotency/ClientRequest.cs
+++ src/Authorization.API/infrastructure/idempotency/ClientRequest.cs
@@ -6,6 +6,6 @@ public class ClientRequest
 {
     public Guid Id { get; set; }
     [Required]
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public DateTime Time { get; set; }
 }
