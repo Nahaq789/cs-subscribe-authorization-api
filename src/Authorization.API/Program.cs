8d34c4107bc8283cef57f1c585575656645935fd
Nahaq789 <naha022677@icloud.com>
2024-06-08 10:45:30 +0000

    認証一旦コメントアウト

--- src/Authorization.API/Program.cs
+++ src/Authorization.API/Program.cs
@@ -9,21 +9,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // JWT
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-#pragma warning disable CS8604 // Possible null reference argument.
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = false,
-            ValidateIssuerSigningKey = true
-        };
-#pragma warning restore CS8604 // Possible null reference argument.
-    });
+    .AddJwtBearer();
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//     .AddJwtBearer(options =>
+//     {
+// #pragma warning disable CS8604 // Possible null reference argument.
+//         options.TokenValidationParameters = new TokenValidationParameters
+//         {
+//             ValidIssuer = builder.Configuration["Jwt:Issuer"],
+//             ValidAudience = builder.Configuration["Jwt:Audience"],
+//             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+//             ValidateIssuer = true,
+//             ValidateAudience = true,
+//             ValidateLifetime = false,
+//             ValidateIssuerSigningKey = true
+//         };
+// #pragma warning restore CS8604 // Possible null reference argument.
+//     });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
