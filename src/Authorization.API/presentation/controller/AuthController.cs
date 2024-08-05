03a4de95855dd7c2d2224a0f8e9fd67fdd55e822
Nahaq789 <naha022677@icloud.com>
2024-08-05 20:10:10 +0900

    feat: jwt refresh controller

--- src/Authorization.API/presentation/controller/AuthController.cs
+++ src/Authorization.API/presentation/controller/AuthController.cs
@@ -55,7 +55,7 @@ public class AuthController : Controller
     ///<param name="command">ユーザーログアウトコマンド</param>
     ///<param name="requestId">リクエストID</param>
     [HttpPost("logout")]
-    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<Results<Ok, BadRequest<string>, ProblemHttpResult>> Logout(
         [FromBody] UserLogoutCommand command, [FromHeader(Name = "x-requestId")] Guid requestId
@@ -71,5 +71,41 @@ public class AuthController : Controller
 
         await _mediator.Send(requestCommand);
         return TypedResults.Ok();
+    }
+
+    /// <summary>
+    /// トークン更新のエンドポイント
+    /// トークン更新ハンドラーを呼び出します。
+    /// </summary>
+    ///<param name="command">トークン更新コマンド</param>
+    ///<param name="requestId">リクエストID</param>
+    [HttpPost("refresh")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<Results<Ok<AuthResult>, BadRequest<string>, ProblemHttpResult>> RefreshJwtToken(
+        [FromHeader(Name = "x-requestId")] Guid requestId,
+        [FromBody] JwtTokenGenerateWhenTokenExpiredCommand command
+    )
+    {
+        try
+        {
+            if (requestId == Guid.Empty) return TypedResults.BadRequest("RequestID is Empty");
+            var requestCommand = new IdentifiedCommand<JwtTokenGenerateWhenTokenExpiredCommand, AuthResult>(command, requestId);
+
+            _logger.LogInformation("Sending command: {CommandName})", requestCommand.GetType());
+
+            var result = await _mediator.Send(requestCommand.Command);
+            return TypedResults.Ok(new AuthResult
+            {
+                Success = true,
+                Token = result.Token,
+                RefreshToken = result.RefreshToken,
+                ErrorMessage = ""
+            });
+        }
+        catch
+        {
+            return TypedResults.Problem(statusCode: 500, detail: "Expired Refresh Token");
+        }
     }
 }
