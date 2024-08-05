7b4fcc4302ca423c401027435b21011055240edf
Nahaq789 <naha022677@icloud.com>
2024-08-05 19:21:44 +0900

    fix: JWTトークンの有効期限が切れた際に、リフレッシュトークンも更新する

--- src/Authorization.API/application/command/JwtTokenGenerateWhenTokenExpiredCommandHandler.cs
+++ src/Authorization.API/application/command/JwtTokenGenerateWhenTokenExpiredCommandHandler.cs
@@ -1,4 +1,5 @@
 using System.IdentityModel.Tokens.Jwt;
+using Authorization.API.application.model;
 using Authorization.API.application.service;
 using Authorization.API.domain.exception;
 using Authorization.API.infrastructure.repository;
@@ -7,7 +8,7 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace Authorization.API.application.command;
 
-public class JwtTokenGenerateWhenTokenExpiredCommandHandler : IRequestHandler<JwtTokenGenerateWhenTokenExpiredCommand, JwtSecurityToken>
+public class JwtTokenGenerateWhenTokenExpiredCommandHandler : IRequestHandler<JwtTokenGenerateWhenTokenExpiredCommand, AuthResult>
 {
     private readonly IUserAuthRepository _userAuthRepository;
     private readonly IJwtTokenService _jwtTokenService;
@@ -20,13 +21,19 @@ public class JwtTokenGenerateWhenTokenExpiredCommandHandler : IRequestHandler<Jw
         this._refreshTokenService = refreshTokenService;
     }
 
-    public async Task<JwtSecurityToken> Handle(JwtTokenGenerateWhenTokenExpiredCommand command, CancellationToken cancellationToken)
+    public async Task<AuthResult> Handle(JwtTokenGenerateWhenTokenExpiredCommand command, CancellationToken cancellationToken)
     {
         var refValid = await _refreshTokenService.ValidateRefreshToken(command.UserId, command.RefreshToken, cancellationToken);
         if (!refValid) throw new SecurityTokenExpiredException("リフレッシュトークンの有効期限が切れています。");
 
-        var user = await _userAuthRepository.GetByEmail(command.Email);
-        if (user == null) throw new UserDomainException("ユーザーが見つかりませんでした。");
-        return _jwtTokenService.GenerateJwtToken(command.Email);
+        var refreshToken = _refreshTokenService.GenerateRefreshToken();
+        var jwtToken = _jwtTokenService.GenerateJwtToken(command.Email);
+
+        return new AuthResult
+        {
+            Success = true,
+            Token = jwtToken.ToString(),
+            RefreshToken = refreshToken,
+        };
     }
 }
